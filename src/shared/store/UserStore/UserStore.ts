d7e358ce2ecc21d98bdb998e6d5ae47fb9d711c2
Aleksey_Levin <raft.took@yandex.ru>
2024-03-08 23:59:34 +0300

    feat: podvyazka

--- src/shared/store/UserStore/UserStore.ts
+++ src/shared/store/UserStore/UserStore.ts
@@ -29,7 +29,7 @@ export class UserStore implements ActivateDeactivate {
   }
 
   mine() {
-    axios.post<IUserData>('/api/endpoint', {
+    axios.post<IUserData>('http://5.42.84.144:8080/api/endpoint', {
       "user_id": this.data?.user?.id.toString()
     }, {
       headers: {
