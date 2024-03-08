8379d28a3b80d24448b2e6c13009351bad96bf6f
Aleksey_Levin <raft.took@yandex.ru>
2024-03-09 01:42:50 +0300

    feat: podvyazka

--- src/shared/store/UserStore/UserStore.ts
+++ src/shared/store/UserStore/UserStore.ts
@@ -2,6 +2,7 @@ import { makeAutoObservable } from 'mobx'
 
 import { ActivateDeactivate } from "../../lib/store/activate.ts"
 import {InitData} from "@tma.js/sdk";
+import axios from "axios";
 
 interface IUserData {
   "earning": string,
@@ -26,24 +27,31 @@ export class UserStore implements ActivateDeactivate {
   async activate(initData?: InitData): Promise<void> {
     this.data = initData
   }
-  mine() {
-    const data = {
-      "user_id": this.data?.user?.id.toString()
-    }
 
-    fetch('http://5.42.84.144:8080/api/endpoint', {
-      method: 'POST',
+  mine() {
+    axios.post<IUserData>('/api/endpoint', {
+      "user_id": this.data?.user?.id.toString()
+    }, {
       headers: {
         Authorization: `Bearer 12345`,
-        'Content-Type': 'application/json;charset=utf-8'
       },
-      referrerPolicy: 'unsafe-url',
-      body: JSON.stringify(data)
-    }).then(async res => {
-      const result = await res.json()
-      console.log(result)
-      this.userData = result
-    })
+    }).then(res => this.userData = res.data)
+    // const data = {
+    //   "user_id": this.data?.user?.id.toString()
+    // }
+    // fetch('https://5.42.84.144:8080/api/endpoint', {
+    //   method: 'POST',
+    //   headers: {
+    //     Authorization: `Bearer 12345`,
+    //     'Content-Type': 'application/json;charset=utf-8'
+    //   },
+    //   referrerPolicy: 'unsafe-url',
+    //   body: JSON.stringify(data)
+    // }).then(async res => {
+    //   const result = await res.json()
+    //   console.log(result)
+    //   this.userData = result
+    // })
   }
 
   reset() {
